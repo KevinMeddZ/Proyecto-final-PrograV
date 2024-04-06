f9e2802eb7bede199701ad7772d68ab12c99d35f
RJMadrigal <josuemm01@hotmail.com>
2024-04-06 14:37:54 -0600

    Version final

--- JS/Funciones.js
+++ JS/Funciones.js
@@ -1,221 +1,109 @@
-
-/*Función para calcular la reserva del vuelo*/
-
 function Calcular() {
-    /**Variable a utilizar en esta clase */
-    var economica, empresarial, primeraClase, excedente, clase, destino, nClase;
-    var temporadaBaja, temporadaEspecial, temporadaAlta, temporada, nTarifa;
-    var cantAdultos, cantNinos;
+    var economica = 0, empresarial = 0.2, primeraClase = 0.45, excedente = 0;
+    var temporadaBaja = 0, temporadaEspecial = 0.2, temporadaAlta = 0.35, temporada = 0;
+    var nTarifa, nClase;
 
-    /**Se obtiene algunos datos */
-    cantAdultos = document.getElementById("txtTiquetes").value;
-    cantNinos = document.getElementById("txtNinos").value;
-    clase = document.querySelector('input[name="op"]:checked').value;
-    fecha = document.getElementById("fechaReservacion");
-    lista = document.getElementById("destinos");
+    var cantAdultos = parseInt(document.getElementById("txtTiquetes").value);
+    var cantNinos = parseInt(document.getElementById("txtNinos").value);
+    var clase = parseInt(document.querySelector('input[name="op"]:checked').value);
+    var fecha = document.getElementById("fechaReservacion").value;
+    var lista = document.getElementById("destinos");
+    var destino = lista.options[lista.selectedIndex].value;
 
-    destino = lista.options[lista.selectedIndex].value;
-
-    /**Se establece el excedente */
-    economica = 0;
-    empresarial = 0.2;
-    primeraClase = 0.45;
-
-    economica = parseFloat(economica);
-    empresarial = parseFloat(empresarial);
-    primeraClase = parseFloat(primeraClase);
-    excedente = parseFloat(excedente);
-
-    cantAdultos = parseInt(cantAdultos);
-    cantNinos = parseInt(cantNinos);
-
-    /**Se establece el % de las temporada */
-    temporadaBaja = 0;
-    temporadaEspecial = 0.2;
-    temporadaAlta = 0.35;
-
-    temporadaBaja = 0;
-    temporadaEspecial = parseFloat(temporadaEspecial);
-    temporadaAlta = parseFloat(temporadaAlta);
-    temporada = parseFloat(temporada);
-
-
-    /**Se pasa la fecha a formato date */
-    var fechaSeleccionada = new Date(fecha.value);
-    /**Se obtiene el mes en numero */
+    var fechaSeleccionada = new Date(fecha);
     var mes = fechaSeleccionada.getMonth() + 1;
-    mes = parseInt(mes);
 
+    if (clase === 1) {
+        clase = economica;
+        nClase = "Economica";
+    } else if (clase === 2) {
+        clase = empresarial;
+        nClase = "Empresarial";
+    } else if (clase === 3) {
+        clase = primeraClase;
+        nClase = "Primera Clase";
+    }
 
-    /**Se obtiene la temporada */
-    if (mes === 4 || mes === 5 || mes === 6 || mes === 8 || mes === 9 || mes === 10 || mes === 11) {
+    if (mes >= 4 && mes <= 11) {
         temporada = temporadaBaja;
         nTarifa = "Baja";
-    } else if (mes === 1 || mes === 2 || mes === 3) {
+    } else if (mes >= 1 && mes <= 3) {
         temporada = temporadaAlta;
         nTarifa = "Alta";
-
     } else if (mes === 7 || mes === 12) {
         temporada = temporadaEspecial;
         nTarifa = "Especial";
-
     }
 
-
-    /**SE OBTIENE LA CLASE */
-    /**Se verifica la clase en que viajan */
-    if (clase == 1) {
-        excedente = economica;
-        nClase = "Clase economica";
-    } else if (clase == 2) {
-        excedente = empresarial;
-        nClase = "Clase empresarial";
-    } else if (clase == 3) {
-        excedente = primeraClase;
-        nClase = "Primera clase";
-    }
-
-
-    /**No funciona, javascript hp */
-    if (cantAdultos === null || cantAdultos === '') {
-        alert("Debes ingresar una cantidad de personas correcta")
-    } else if (cantNinos === null || cantNinos === '') {
+    if (isNaN(cantAdultos) || cantAdultos === '') {
+        alert("Debes ingresar una cantidad de personas correcta");
+    } else if (isNaN(cantNinos)) {
         cantNinos = 0;
     }
 
-
-    /**Funcion que calcula todo */
     if ((cantAdultos + cantNinos) <= 6) {
-        CalcularTotal(destino, temporada, excedente, nClase, nTarifa);
+        CalcularTotal(destino, temporada, clase, excedente, nClase, nTarifa);
     } else {
-        alert("Cantidad de personas máximas: 6")
+        alert("Cantidad de personas máximas: 6");
     }
-
-
-
-
-
-
 }
 
-
-function CalcularTotal(destino, temporada, excedente, nClase, nTarifa) {
-    var nombre, cedula, email, telefono, destino, lista, cantTickets, cantNinos, fecha, totalAdulto, totalNinos;
+function CalcularTotal(destino, temporada, clase, excedente, nClase, nTarifa) {
+    var nombre, cedula, email, telefono, lista, cantTickets, cantNinos, fecha, totalAdulto, totalNinos;
     var tarifaUsa, tarifaPeru, tarifaEspana, tarifaPanama, tarifaGuatemala;
-    var totalPagar;
+    var subtotal =0;
+    var excedente =0;
+    var totalPagar = 0;
+    var iv = 0;
 
-
-    /**Se obtiene los datos del formulario */
     nombre = document.getElementById("txtNombre").value;
     cedula = document.getElementById("txtCedula").value;
     email = document.getElementById("txtEmail").value;
     telefono = document.getElementById("txtNumero").value;
     lista = document.getElementById("destinos");
     destino = lista.options[lista.selectedIndex].value;
-    cantTickets = document.getElementById("txtTiquetes").value;
-    cantNinos = document.getElementById("txtNinos").value;
+    cantTickets = parseInt(document.getElementById("txtTiquetes").value);
+    cantNinos = parseInt(document.getElementById("txtNinos").value);
     fecha = document.getElementById("fechaReservacion").value;
 
-    /**Formato a los enteros */
-    cedula = parseInt(cedula);
-    telefono = parseInt(telefono);
-    cantTickets = parseInt(cantTickets);
-    cantNinos = parseInt(cantNinos);
-    totalAdulto = parseInt(totalAdulto);
-    totalNinos = parseInt(totalNinos);
-    totalPagar = parseInt(totalPagar);
-
-
-    /**Se establece las tarifas de los países */
     tarifaUsa = 250;
     tarifaPeru = 500;
     tarifaEspana = 1000;
     tarifaPanama = 300;
     tarifaGuatemala = 4000;
 
-    tarifaUsa = parseInt(tarifaUsa);
-    tarifaPeru = parseInt(tarifaPeru);
-    tarifaEspana = parseInt(tarifaEspana);
-    tarifaPanama = parseInt(tarifaPanama);
-    tarifaGuatemala = parseInt(tarifaGuatemala);
-
-
-    /**Se realiza los cálculos */
-    /**Se verifica el destino */
     switch (destino) {
         case "Peru":
-            /**ADULTOS */
-            totalAdulto = (cantTickets * tarifaPeru);
-            /**Se le agrega el % por la temporada */
-            totalAdulto = totalAdulto + (totalAdulto * temporada);
-            /**Se le agrega % del excedente */
-            totalAdulto = totalAdulto + (totalAdulto * excedente);
-
-
-            /**NIÑOS(A) */
-            totalNinos = (cantNinos * tarifaPeru) * 0.4;
-            /**Se le agrega el % por la temporada */
-            totalNinos = totalNinos + (totalNinos * temporada);
-            totalNinos = totalNinos + (totalNinos * excedente);
+            totalAdulto = cantTickets * tarifaPeru * (1 + temporada + excedente);
+            totalNinos = cantNinos * (tarifaPeru * 0.4) * (1 + temporada + excedente);
+            break;
         case "USA":
-            /**ADULTOS */
-            totalAdulto = cantTickets * tarifaUsa;
-            /**Se le agrega el % por la temporada */
-            totalAdulto = totalAdulto + (totalAdulto * temporada);
-            /**Se le agrega % del excedente */
-            totalAdulto = totalAdulto + (totalAdulto * excedente);
-
-
-            /**NIÑOS(A) */
-            totalNinos = (cantNinos * tarifaUsa) * 0.4;
-            /**Se le agrega el % por la temporada */
-            totalNinos = totalNinos + (totalNinos * temporada);
-            totalNinos = totalNinos + (totalNinos * excedente);
+            totalAdulto = cantTickets * tarifaUsa * (1 + temporada + excedente);
+            totalNinos = cantNinos * (tarifaUsa * 0.4) * (1 + temporada + excedente);
+            break;
         case "España":
-            /**ADULTOS */
-            totalAdulto = cantTickets * tarifaEspana;
-            /**Se le agrega el % por la temporada */
-            totalAdulto = totalAdulto + (totalAdulto * temporada);
-            /**Se le agrega % del excedente */
-            totalAdulto = totalAdulto + (totalAdulto * excedente);
-
-            /**NIÑOS(A) */
-            totalNinos = (cantNinos * tarifaEspana) * 0.4;
-            /**Se le agrega el % por la temporada */
-            totalNinos = totalNinos + (totalNinos * temporada);
-            totalNinos = totalNinos + (totalNinos * excedente);
+            totalAdulto = cantTickets * tarifaEspana * (1 + temporada + excedente);
+            totalNinos = cantNinos * (tarifaEspana * 0.4) * (1 + temporada + excedente);
+            break;
         case "Panama":
-            /**ADULTOS */
-            totalAdulto = cantTickets * tarifaPanama;
-            /**Se le agrega el % por la temporada */
-            totalAdulto = totalAdulto + (totalAdulto * temporada);
-            /**Se le agrega % del excedente */
-            totalAdulto = totalAdulto + (totalAdulto * excedente);
-
-            /**NIÑOS(A) */
-            totalNinos = (cantNinos * tarifaPanama) * 0.4;
-            /**Se le agrega el % por la temporada */
-            totalNinos = totalNinos + (totalNinos * temporada);
-            totalNinos = totalNinos + (totalNinos * excedente);
+            totalAdulto = cantTickets * tarifaPanama * (1 + temporada + excedente);
+            totalNinos = cantNinos * (tarifaPanama * 0.4) * (1 + temporada + excedente);
+            break;
         case "Guatemala":
-            /**ADULTOS */
-            totalAdulto = cantTickets * tarifaGuatemala;
-            /**Se le agrega el % por la temporada */
-            totalAdulto = totalAdulto + (totalAdulto * temporada);
-            /**Se le agrega % del excedente */
-            totalAdulto = totalAdulto + (totalAdulto * excedente);
-
-            /**NIÑOS(A) */
-            totalNinos = (cantNinos * tarifaGuatemala) * 0.4;
-            /**Se le agrega el % por la temporada */
-            totalNinos = totalNinos + (totalNinos * temporada);
-            totalNinos = totalNinos + (totalNinos * excedente);
+            totalAdulto = cantTickets * tarifaGuatemala * (1 + temporada + excedente);
+            totalNinos = cantNinos * (tarifaGuatemala * 0.4) * (1 + temporada + excedente);
+            break;
     }
 
-    /**Total a pagar */
-    totalPagar = totalAdulto + totalNinos;
+    console.log(clase.toString());
 
-    /*Variables de sesión*/
+
+    subtotal = totalAdulto + totalNinos;
+    excedente = subtotal * clase;
+    iv = subtotal * 0.13;
+    totalPagar =  subtotal + excedente + iv;
+
+
     sessionStorage.setItem("s_nombre", nombre);
     sessionStorage.setItem("s_cedula", cedula);
     sessionStorage.setItem("s_telefono", telefono);
@@ -227,24 +115,22 @@ function CalcularTotal(destino, temporada, excedente, nClase, nTarifa) {
     sessionStorage.setItem("s_adultos", cantTickets);
     sessionStorage.setItem("s_totalAdulto", totalAdulto);
     sessionStorage.setItem("s_totalNinos", totalNinos);
-    sessionStorage.setItem("s_totalPagar", totalPagar);
+    sessionStorage.setItem("s_subtotal", subtotal);
+    sessionStorage.setItem("s_iv", iv.toFixed(2));
+    sessionStorage.setItem("s_excedente", excedente.toFixed(2));
+    sessionStorage.setItem("s_totalPagar", totalPagar.toFixed(2));
 
     document.location.href = "pago.html";
 }
 
-
-/**Obtiene los datos en la otra página */
-
 function ObtenerDatos() {
     var fechaHoy = new Date();
     var dia = fechaHoy.getDate();
-    var mes = fechaHoy.getMonth() + 1; // Los meses se indexan desde 0, por lo que se suma 1
+    var mes = fechaHoy.getMonth() + 1;
     var anio = fechaHoy.getFullYear();
 
-    // Formatear la fecha como "YYYY-MM-DD" para que sea compatible con el input de tipo date
     var fechaFormateada = anio + '-' + (mes < 10 ? '0' : '') + mes + '-' + (dia < 10 ? '0' : '') + dia;
 
-    // Establecer el valor del input de tipo date
     document.getElementById('fechaPago').value = fechaFormateada;
     document.getElementById("txtNombre").value = sessionStorage.getItem("s_nombre");
     document.getElementById("txtCedula").value = sessionStorage.getItem("s_cedula");
@@ -257,5 +143,19 @@ function ObtenerDatos() {
     document.getElementById("txtClase").value = sessionStorage.getItem("s_clase");
     document.getElementById("txtMontoAdulto").value = sessionStorage.getItem("s_totalAdulto");
     document.getElementById("txtMontoNinos").value = sessionStorage.getItem("s_totalNinos");
+    document.getElementById("txtSubTotal").value = sessionStorage.getItem("s_subtotal");
+    document.getElementById("txtIV").value = sessionStorage.getItem("s_iv");
+    document.getElementById("txtExcedente").value = sessionStorage.getItem("s_excedente");
     document.getElementById("txtTotal").value = sessionStorage.getItem("s_totalPagar");
+}
+
+function validarFormulario() {
+    var form = document.getElementById('formulario');
+    if (form.checkValidity()) {
+        // Si el formulario es válido, realiza la acción deseada, por ejemplo, enviar los datos.
+        Calcular();
+    } else {
+        // Si el formulario no es válido, muestra un mensaje de error o realiza alguna otra acción.
+        alert('Por favor, completa todos los campos correctamente.');
+    }
 }
